05ad253634495f9a0ca7d989b2f8ec46fe73f299
Giorgio <tran.giorgio@gmail.com>
2022-06-01 13:45:30 -1000

    Completed courseinfo

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,64 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const Header = (props) => {
+    return (
+      <div>
+        <p>
+          {props.course}
+        </p>
+      </div>
+    )
+  }
+  
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Part = (props) => {
+    return (
+      <div>
+        <p>
+          {props.part} {props.exercise}
+        </p>
+      </div>
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <Part part={props.part1} exercise={props.exercises1}/>
+        <Part part={props.part2} exercise={props.exercises2}/>
+        <Part part={props.part3} exercise={props.exercises3}/>
+      </div>
+    )
+  }
+
+  const total = exercises1 + exercises2 + exercises3
+  const Total = (props) => {
+    return (
+      <div>
+        <p>
+          Number of exercises {props.total}
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content 
+        part1={part1} exercises1={exercises1} 
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total total={total}/>
+    </div>
+  )
+}
+
+export default App
