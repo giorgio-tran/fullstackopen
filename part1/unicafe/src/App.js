b02d6b3a209f5d390be6721ae2964015da076096
Giorgio <tran.giorgio@gmail.com>
2022-06-04 23:19:54 -1000

    Completed exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,44 +12,62 @@ const Header = props => (
   <h1>{props.text}</h1>
 )
 
-const Rate = props => (
-  <p>{props.rating} {props.count}</p>
+const Div = props => (
+  <div>{props.text} {props.number}</div>
 )
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [good, setGood] = useState({
+    value: 1, count: 0
+  })
+  const [neutral, setNeutral] = useState({
+    value: 0, count: 0
+  })
+  const [bad, setBad] = useState({
+    value: -1, count: 0
+  })
+  const [allClicks, setAll] = useState({
+    value: [], count: 0
+  })
 
   //good
-  const setToGood = () => {
-    console.log('good', good)
-    setGood(good + 1)
+  const handleGood = () => {
+    console.log('value', good.value)
+    console.log('count', good.count)
+    setGood({value: good.value, count: good.count + 1})
+    console.log('allClicks val', allClicks.value)
+    console.log('allClicks count', allClicks.count)
+    setAll({value: allClicks.value.concat(good.value), count: allClicks.count + 1})
   }
   //neutral
-  const setToNeutral = () => {
-    console.log('neutral', neutral)
-    setNeutral(neutral + 1)
+  const handleNeutral = () => {
+    setNeutral({value: neutral.value, count: neutral.count + 1})
+    setAll({value: allClicks.value.concat(neutral.value), count: allClicks.count + 1})
   }
-
   //bad
-  const setToBad = () => {
-    console.log('bad', bad)
-    setBad(bad + 1)
+  const handleBad = () => {
+    setBad({value: bad.value, count: bad.count + 1})
+    setAll({value: allClicks.value.concat(bad.value), count: allClicks.count + 1})
   }
+  //all
+  const average = allClicks.value.reduce((a, b) => a + b, 0) / allClicks.value.length
+  //positive reviews
+  const positive =  good.count / allClicks.value.length
 
   return (
     <div>
       <Header text="Give Feedback" />
-      <Button onClick={setToGood} text="good"/>
-      <Button onClick={setToNeutral} text="neutral" />
-      <Button onClick={setToBad} text="bad"/>
+      <Button onClick={handleGood} text="good"/>
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad"/>
       <Header text="Statistics" />
-      <Rate rating="good" count={good} />
-      <Rate rating="neutral" count={neutral} />
-      <Rate rating="bad" count={bad} />
+      <Div text="good" number={good.count} />
+      <Div text="neutral" number={neutral.count} />
+      <Div text="bad" number={bad.count} />
+      <Div text="all" number={allClicks.count} />
+      <Div text="average" number={average} />
+      <Div text="positive" number={[positive, ' %']} />
     </div>
   )
 }
