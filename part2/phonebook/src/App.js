c69a80f35862ec8d3513a4bdb68b2e2e5c61df81
Giorgio <tran.giorgio@gmail.com>
2022-06-10 09:29:19 -1000

    Complete exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,8 +9,18 @@ const App = () => {
   const addName = (event) => {
     event.preventDefault()
     const nameObj = {name: newName}
-    setPersons(persons.concat(nameObj))
-    setNewName('')
+    //checks to see if the person is already on the list
+    const duplicate = persons.filter((person) =>
+      person.name === nameObj.name
+    )
+    console.log(duplicate)
+    //checks for duplicate
+    if (duplicate.length > 0) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(nameObj))
+      setNewName('')
+    }
     console.log(persons)
   }
   //for input's onChange
