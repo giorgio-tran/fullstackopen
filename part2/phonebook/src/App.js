b812bac322239ef85ff41f5a97827eb47de4807d
Giorgio <tran.giorgio@gmail.com>
2022-06-10 10:09:01 -1000

    Completed exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,47 +2,66 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '8081234567'}
   ]) 
   const [newName, setNewName] = useState('')
-  //adds name to persons
-  const addName = (event) => {
+  const [newNumber, setNewNumber] = useState('')
+
+  //adds person's name 
+  const addContact = (event) => {
     event.preventDefault()
-    const nameObj = {name: newName}
+    const contactObj = {name: newName, number: newNumber}
     //checks to see if the person is already on the list
     const duplicate = persons.filter((person) =>
-      person.name === nameObj.name
+      person.name === contactObj.name
     )
-    console.log(duplicate)
-    //checks for duplicate
+    //checks for duplicate in the list
     if (duplicate.length > 0) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat(nameObj))
+      //adds contact to the list
+      setPersons(persons.concat(contactObj))
       setNewName('')
+      setNewNumber('')
     }
-    console.log(persons)
+    console.log('persons', persons)
   }
-  //for input's onChange
+
+  //for input's onChange of name
   const handleNameChange =(event) => {
     setNewName(event.target.value)
   }
+
+  //for input's onChange of number
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   //prints each element in persons
   const nameList = persons.map((person) =>
       <div key={person.name}>
-        {person.name}
+        {person.name} {person.number}
       </div>
   )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addContact}>
         <div>
           name: 
           <input 
             value={newName}
             onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            type="number"
+            onChange={handleNumberChange}
           />
         </div>
         <div>
