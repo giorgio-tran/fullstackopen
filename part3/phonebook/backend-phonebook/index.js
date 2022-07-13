660dc8bc60a2ba065720b9bce676ebaf56302ee0
Giorgio <tran.giorgio@gmail.com>
2022-07-13 10:00:11 -1000

    Completed exercise 3.16 and 3.17

--- part3/phonebook/backend-phonebook/index.js
+++ part3/phonebook/backend-phonebook/index.js
@@ -52,6 +52,23 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+//updates person in MongoDB
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+
+})
+
 //deletes person in MongoDB
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndRemove(request.params.id)
