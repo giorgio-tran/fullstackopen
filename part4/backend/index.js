e1887ec7ff4aaa062a498b8d865debed66c64d65
Giorgio <tran.giorgio@gmail.com>
2022-08-01 18:12:12 -1000

    Fix typos and refactor index.js

--- part4/backend/index.js
+++ part4/backend/index.js
@@ -1,51 +1,10 @@
-require('dotenv').config()
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const Blog = require('./models/blog') 
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(express.json())
+const server = http.createServer(app)
 
-app.get('/', (req, res) => {
-    res.send('<h1> Hello World! </h1>')
-})
-
-//gets all of the blogs via find method in DB, converts data to json
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const body = request.body
-
-    if (!(body.title && body.author && body.url && body.likes)) {
-        console.log(body)
-        return response.status(400).json({
-            error: 'content is missing'
-        })
-    }
-
-    //creates a blog object based on schema
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-    })
-    //saves to the database or catches error
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-        .catch(error => console.log('ERROR:', error))
-})
-
-const PORT = 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
