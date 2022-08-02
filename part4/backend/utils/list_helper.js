420edc011303778b126d553d70e9f2e2422b0aa2
Giorgio <tran.giorgio@gmail.com>
2022-08-02 09:43:12 -1000

    Complete 4.3 and 4.4

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length == 0) {
+        return 0
+    }
+    if (blogs.length == 1) {
+        return blogs[0].likes
+    }
+    return blogs.reduce(
+        (previousValue, currentValue) => {
+            return previousValue + currentValue.likes
+        }, 0
+    )
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
