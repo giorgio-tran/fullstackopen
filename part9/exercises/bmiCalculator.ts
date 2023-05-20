988fa290aa89eaa1012cacb5dc195899c4de2e11
Giorgio <tran.giorgio@gmail.com>
2023-05-20 13:43:15 -1000

    Complete 9.3

--- part9/exercises/bmiCalculator.ts
+++ part9/exercises/bmiCalculator.ts
@@ -1,5 +1,24 @@
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseBmiArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (isNaN(Number(args[2])) && isNaN(Number(args[3]))) {
+    throw new Error('Invalid arguments, not numbers!');
+  }
+  return {
+    height: Number(args[2]),
+    weight: Number(args[3]),
+  }
+}
+
 const calculateBmi = (height: number, weight: number): string => {
-  const bmi = height / (weight * weight);
+  const heightInMeters = height/100;
+  const bmi = weight / (heightInMeters * heightInMeters);
 
   if (bmi < 18.5) return 'Underweight';
   if (bmi < 23) return 'Normal (healthy weight)';
@@ -7,4 +26,13 @@ const calculateBmi = (height: number, weight: number): string => {
   return 'Very overweight';
 }
 
-console.log(calculateBmi(180, 74));
+try {
+  const { height, weight } = parseBmiArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = 'Error: ';
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
