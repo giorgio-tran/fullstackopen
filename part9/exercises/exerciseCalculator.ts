46ddddfbe557ad4037860fe541130da0247cc790
Giorgio <tran.giorgio@gmail.com>
2023-05-20 13:15:58 -1000

    Implement arg parsing for exercise calculator

--- part9/exercises/exerciseCalculator.ts
+++ part9/exercises/exerciseCalculator.ts
@@ -8,6 +8,36 @@ interface Result {
   average: number;
 }
 
+interface ExerciseValues {
+  target: number;
+  arr: number[];
+}
+
+const isNumber = (arg: string): boolean => {
+  return !isNaN(Number(arg));
+}
+
+const parseArguments = (args: string[]): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 12) throw new Error('Too many arguments');
+
+  const temp = args.slice(3);
+
+  if (!isNumber(args[2])) throw new Error('Target value is not a number');
+
+  const argArr = temp.map((value: string) => {
+    if (!isNumber(value)) {
+      throw new Error(`${value} is not a number!`);
+    }
+    return Number(value);
+  })
+
+  return {
+    target: Number(args[2]),
+    arr: argArr
+  }
+}
+
 const calculateExercises = (arr: number[], target: number): Result => {
   let offDays = 0;
   let totalHoursTrained = 0;
@@ -43,4 +73,15 @@ const calculateExercises = (arr: number[], target: number): Result => {
 
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+  const { target, arr } = parseArguments(process.argv);
+  console.log(calculateExercises(arr, target));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
+
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
